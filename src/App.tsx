82ab89e47da90c1207a543fb768f9441c6684621
Jailson Prado <jailsondpd@gmail.com>
2022-04-09 15:37:31 -0300

    new repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../src/logop.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Typescript com React</h1>
+    </div>
+  );
+}
+
+export default App;
